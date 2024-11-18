3b24437c4616c306130d98cb719de236cc3cf5a5
jade-banares <jade.arinal.banares@gmail.com>
2024-11-18 15:25:43 +0800

    change style & js

--- src/js/data.js
+++ src/js/data.js
@@ -1,30 +1,30 @@
-const advice = document.getElementById("advice");
-    const blockQuote = document.getElementById("quotes");
-    const btnClick = document.getElementById("btn");
+const advice = document.getElementById("advice")
+const blockQuote = document.getElementById("quotes")
+const btnClick = document.getElementById("btn")
 
-    // Function to fetch advice
-    function fetchAdvice() {
-      const apiUrl = `https://api.adviceslip.com/advice?timestamp=${new Date().getTime()}`;
-      fetch(apiUrl)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("Network error");
-          }
-          return response.json();
-        })
-        .then((data) => {
-          advice.textContent = `${data.slip.id}`;
-          blockQuote.textContent = `${data.slip.advice}`;
-        })
-        .catch((error) => {
-          console.error("Error fetching data:", error);
-        });
-    }
+// Function to fetch advice
+function fetchAdvice() {
+  const apiUrl = `https://api.adviceslip.com/advice?timestamp=${new Date().getTime()}`
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network error")
+      }
+      return response.json()
+    })
+    .then((data) => {
+      advice.textContent = `ADVICE #${data.slip.id}`
+      blockQuote.textContent = `“${data.slip.advice}”`
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error)
+    })
+}
 
-    // Fetch advice when the page loads
-    fetchAdvice();
+// Fetch advice when the page loads
+fetchAdvice()
 
-    // Fetch advice when the button is clicked
-    btnClick.addEventListener("click", function () {
-      fetchAdvice();
-    });
+// Fetch advice when the button is clicked
+btnClick.addEventListener("click", function () {
+  fetchAdvice()
+})
